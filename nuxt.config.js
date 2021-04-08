ffa1848e5303bd318b1671f92394ab19be55d913
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2021-04-08 11:55:10 +0200

    Added image preview

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Agustín Millán Jiménez\'s Portfolio' },
       { hid: 'og:title', name: 'og:title', content: 'Portfolio' },
-      { hid: 'og:image', name: 'og:image', content: '/software-engineer.svg' }
+      { hid: 'og:image', name: 'og:image', content: '/Profile.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
